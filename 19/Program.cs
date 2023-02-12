a80d33f63f378451bed8dac9ed178945f3673027
Mary <of51@mail.ru>
2023-02-12 17:18:48 +0300

    Домашка 3 язык C#

---
+++ 19/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает 
+//на вход пятизначное число и проверяет, является ли 
+//оно палиндромом.
+
+Console.Clear();
+Console.Write("ВВедите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+int num = number;
+int rev = 0;
+
+if (number <10000 || number > 99999)
+{
+    Console.WriteLine("Введено не правильное число");
+    return;
+}
+while (number > 0)
+{
+    int i = number % 10;
+    rev = rev * 10 + i;
+    number = number / 10;
+}
+if (num == rev)
+{
+    Console.WriteLine("Это палиндром! ");
+} else
+{
+    Console.WriteLine("Это не палиндром! ");
+}
---
+++ 23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на 
+//вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27. 
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int m = N; // m = -10
+Console.Write("Таблица кубов чисел от 1 до N: ");
+if (N < 0)
+{
+    i = N; //i = -10
+    m = -N;// m = 10
+    N = -1; // N = -1
+}
+int[] result = new int[m];
+for (int j = 0; i <= N; i++)
+{
+    result[j] = i * i * i;
+    j++;
+}
+for (int j = 0; j < m; j++)
+{
+        Console.Write($"{result[j]} ");
+}
+
